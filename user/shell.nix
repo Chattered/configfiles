ee563653e48ba758e0108c17a9b3f632e62ef483
Phil Scott <me@philscotted.com>
2016-09-14 15:20:18 +0100

    w3m currently not compiling

--- user/shell.nix
+++ user/shell.nix
@@ -4,7 +4,7 @@
    emacs = 
      let myemacs =
        with pkgs.emacsPackages; with pkgs.emacsPackagesNg; with pkgs.emacsMelpa; pkgs.emacsWithPackages
-       [ cl-lib helm-projectile magit org paredit emacsw3m ];
+       [ cl-lib helm-projectile magit org oauth2 paredit twittering-mode ];
      in pkgs.stdenv.mkDerivation {
        name = "emacs";
        buildInputs = [ myemacs pkgs.mu pkgs.offlineimap ];
@@ -18,8 +18,7 @@
 	        (expand-file-name \"emacs-init.org\" (getenv \"HOME\")))\
              (switch-to-buffer \"*scratch*\")\
  	     (require 'helm-config)
-             (paredit-mode)
- 	     (require 'w3m))"
+             (paredit-mode))"
          fi
        '';
      };
