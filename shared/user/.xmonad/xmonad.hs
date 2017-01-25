5fb528ad9a84ee84f5f8a5f969f2cf1720397724
Phil Scott <me@philscotted.com>
2017-01-25 20:52:34 +0000

    Fullscreen support. Cycle kb layouts. Change browsers. Alsa update.

--- shared/user/.xmonad/xmonad.hs
+++ shared/user/.xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import Alsa
 import Control.Applicative
 import Control.Monad
 import Data.Ix
+import Data.IORef
 import System.Posix.Env (getEnv, setEnv)
 import System.FilePath.Posix ((</>))
 import System.Exit
@@ -12,6 +13,8 @@ import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
 
 onPlayback :: (Elem -> Alsa a) -> Alsa a
 onPlayback f = do
@@ -49,22 +52,28 @@ muteVolume = onPlayback $ \p -> do
 main = do
   Just home <- getEnv "HOME"
   setEnv "NIX_PATH" (home </> "unstable") False
+  kbLayouts <- newIORef (cycle ["dvorak","us"])
+  let nextLayout = fmap head (readIORef kbLayouts) <* modifyIORef kbLayouts tail
+  let doNextLayout = liftIO nextLayout >>= spawn . ("setxkbmap " ++)
   h <- spawnPipe "xmobar"
-  xmonad $ defaultConfig
+  xmonad $ ewmh defaultConfig
         { logHook    = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn h }
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , layoutHook = smartBorders(avoidStruts $ layoutHook defaultConfig)
         , manageHook = manageHook defaultConfig <+> manageDocks
+        , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
         , modMask = mod4Mask
         , terminal = "xterm"
         } `additionalKeys`
-       [ ((mod4Mask .|. shiftMask, xK_f),
+       [ ((mod4Mask .|. shiftMask, xK_t),
+          spawn "nix-shell -p torbrowser --run tor-browser")
+       , ((mod4Mask .|. shiftMask, xK_f),
           spawn "nix-shell -p firefox --run firefox")
-       , ((mod4Mask .|. shiftMask, xK_g),
-          spawn "nix-shell -p google-chrome --run google-chrome-stable")    
        , ((mod4Mask .|. shiftMask, xK_m),
           spawn "nix-shell -A emacs --run 'emacsclient -c'")
-       , ((mod4Mask,               xK_u), liftIO . runAlsa $ upVolume)
-       , ((mod4Mask,               xK_d), liftIO . runAlsa $ downVolume)
-       , ((mod4Mask .|. shiftMask, xK_d), liftIO . runAlsa $ muteVolume)
+       , ((mod4Mask,               xK_s), doNextLayout)
+       , ((mod4Mask,               xK_semicolon), doNextLayout)
+       , ((mod4Mask,               xK_u), liftIO . runAlsa "hw:0" $ upVolume)
+       , ((mod4Mask,               xK_d), liftIO . runAlsa "hw:0" $ downVolume)
+       , ((mod4Mask .|. shiftMask, xK_d), liftIO . runAlsa "hw:0" $ muteVolume)
        , ((mod4Mask              , xK_x), spawn "xlock -mode blank")
        ]
