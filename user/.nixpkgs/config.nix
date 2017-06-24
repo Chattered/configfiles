cb9b7baecdea6078fd0c6b65f65dc3a91b2b353e
Phil Scott <me@philscotted.com>
2017-06-24 18:41:04 +0100

    Update emacs packages.

--- user/.nixpkgs/config.nix
+++ user/.nixpkgs/config.nix
@@ -30,29 +30,27 @@ in rec {
         withGTK2 = false;
         withGTK3 = false;
       };
-      emacsPackagesNg = pkgs.emacsPackagesNg.override (super: self: {
-          twittering-mode = self.melpaPackages.twittering-mode;
-          # This is the melpa package as defined in nixpkgs master. We can change to
-          # w3m = self.melpaPackages.w3m once it makes it into unstable.
-          w3m = self.callPackage ({ fetchFromGitHub, fetchurl, lib, melpaBuild }:
-            melpaBuild {
-                pname = "w3m";
-                version = "20121224.2047";
-                src = fetchFromGitHub {
-                  owner = "emacsorphanage";
-                  repo = "w3m";
-                  rev = "5986b51c7c77500fee3349fb0b3f4764d3fc727b";
-                  sha256 = "1lgvdaghzj1fzh8p6ans0f62zg1bfp086icbsqmyvbgpgcxia9cs";
-                };
-                recipeFile = fetchurl {
-                  url = "https://raw.githubusercontent.com/milkypostman/melpa/50e8d089f4e163eb459fc602cb90440b110b489f/recipes/w3m";
-                  sha256 = "0vh882b44vxnij3l01sig87c1jmbymgirf6s98mvag1p9rm8agxw";
-                  name = "w3m";
-                };
-                packageRequires = [ pkgs.w3m ];
-                meta = {
-                  homepage = "https://melpa.org/#/w3m";
-                  license = lib.licenses.free;
+      emacsPackagesNg = pkgs.emacsPackagesNg.overrideScope (super: self: {
+        w3m = pkgs.emacsPackagesNg.callPackage
+         ({ fetchFromGitHub, fetchurl, lib, melpaBuild }:
+          melpaBuild {
+              pname = "w3m";
+              version = "20121224.2047";
+              src = fetchFromGitHub {
+                owner = "emacsorphanage";
+                repo = "w3m";
+                rev = "5986b51c7c77500fee3349fb0b3f4764d3fc727b";
+                sha256 = "1lgvdaghzj1fzh8p6ans0f62zg1bfp086icbsqmyvbgpgcxia9cs";
+              };
+              recipeFile = fetchurl {
+                url = "https://raw.githubusercontent.com/milkypostman/melpa/50e8d089f4e163eb459fc602cb90440b110b489f/recipes/w3m";
+                sha256 = "0vh882b44vxnij3l01sig87c1jmbymgirf6s98mvag1p9rm8agxw";
+                name = "w3m";
+              };
+              packageRequires = [ pkgs.w3m ];
+              meta = {
+                homepage = "https://melpa.org/#/w3m";
+                license = lib.licenses.free;
                 };
               }) {};
         });
